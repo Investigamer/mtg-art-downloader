55fb06c28444d0e3d2ab19948bcdf7d746bcaac2
MrTeferi <92750180+MrTeferi@users.noreply.github.com>
2021-12-06 01:12:46 -0600

    Add files via upload

--- find.py
+++ find.py
@@ -23,7 +23,7 @@ Path(settings.f_scry_b).mkdir(mode=511, parents=True, exist_ok=True)
 z = 0
 while z == 0:
 	# Does the user want to use Google Sheet queeries or cards from txt file?
-	print("Enter 1 to download images from Google Sheet queery.\nEnter 2 to download images from cards.txt list.")
+	print("Enter 1 to download images from Google Sheet query.\nEnter 2 to download images from cards.txt list.")
 	choice = input()
 	
 	# Import our Google Sheets generated queeries
@@ -32,7 +32,6 @@ while z == 0:
 		print("Can do! Grab yourself a beer, we might be here a minute..")
 		import sheet_generated
 	elif choice == "2":
-		z = 1
-		print("Can do! Grab yourself a beer, we might be here a minute..")
+		z = "Loading decklist! Grab yourself a beer, we might be here a minute..")
 		import txt_downloader
 	else: print("Do you think this is a game? Try again.\n")
