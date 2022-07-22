cd001ca0141b4f15b924e0e146d161c6d683a6ff
MrTeferi <freethoughtleft@gmail.com>
2022-07-22 05:20:51 -0500

    Update build.py
    - Added scryfall.json to build script

--- build.py
+++ build.py
@@ -25,6 +25,7 @@ files = [
     {'src': os.path.join(MTG, 'codes.json'), 'dst': os.path.join(DIST_MTG, 'codes.json')},
     {'src': os.path.join(MTG, 'links.json'), 'dst': os.path.join(DIST_MTG, 'links.json')},
     {'src': os.path.join(MTG, 'special.json'), 'dst': os.path.join(DIST_MTG, 'special.json')},
+    {'src': os.path.join(MTG, 'scryfall.json'), 'dst': os.path.join(DIST_MTG, 'scryfall.json')},
 ]
 
 # Folders that need to be copied
